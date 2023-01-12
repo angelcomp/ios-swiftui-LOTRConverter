590d9f54e7e76d3c5a80f5fd8e839cabac225895
Angelica dos Santos <santosangelicassp@gmail.com>
2023-01-12 14:20:43 -0300

    main screen

--- ios-swiftui-LOTRConverter/ContentView.swift
+++ ios-swiftui-LOTRConverter/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var leftAmount = ""
+    @State private var rightAmount = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "heart")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Angelica!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    VStack {
+                        HStack {
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            Text("Silver Piece")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                        }
+                        .padding(.bottom, -5)
+                        
+                        TextField("Amount", text: $leftAmount)
+                            .padding(7)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(7)
+                    }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                    VStack {
+                        HStack {
+                            Text("Gold Piece")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                            
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        .padding(.bottom, -5)
+                        
+                        TextField("Amount", text: $rightAmount)
+                            .padding(10)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(7)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .cornerRadius(50)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                }
+                .padding(.trailing)
+            }
         }
-        .padding()
     }
 }
 
